fea8f10471ce13b8466c6fcf849183f42ce3735a
Luca <Luca@MacBook-Pro-di-Apple-7.local>
2019-04-15 19:53:57 +0200

    Aggiunta configurazione al database con PDO

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,11 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+$container ['db'] = function ($c) {
+	$settings = $c -> ('settings')['db'];
+	$pdo = new PDO ('mysql:host=:' - $settings ['host'] . ';port=' . $settings ['port'] . ';dbname=' . $settings ['dbname'], $settings ['user'], $settings ['pass']);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	return $pdo;
+};
